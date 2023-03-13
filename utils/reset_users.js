49cf69ffbe86ce5c07c51c2b6990ae3c97c5b65a
Maxim Veksler <maximvek05@gmail.com>
2023-03-13 17:29:32 -0400

    fix reset_users.js code

--- utils/reset_users.js
+++ utils/reset_users.js
@@ -13,10 +13,28 @@ mongo.connect("mongodb://localhost:27017", {useNewUrlParser: true, useUnifiedTop
     let general_room = await database.collection("rooms").findOne({name:"general"});
 
     let general_users = []
+
     for (let i = 0; i < users.length; i++) {
         general_users.push(users[i].username)
-        let result = await database.collection("users").updateOne({username: users[i].username}, {$push: {rooms: {room_name: "general", room_id: ObjectID(general_room._id)}}});
-        console.log(result);
+
+        user = users[i];
+
+        let userGeneralRooms = user.rooms.find(x => x.room_name === "general");
+
+        if (userGeneralRooms.length > 1) {
+            for (let j = 1; j < userGeneralRooms.length; j++) {
+                user.rooms.splice(user.rooms.indexOf(userGeneralRooms[j]), 1);
+            }
+        } else if (userGeneralRooms.length === 0) {
+            let result = await database.collection("users").updateOne({username: users[i].username}, {$push: {rooms: {room_name: "general", room_id: ObjectID(general_room._id)}}});
+            console.log(`added ${user.username} to general room`);
+            // console.log(result);
+        }
+
+        // shift general to be first in array of rooms
+        let indexOfGeneral = user.rooms.findIndex(x => x.room_name === "general");
+        user.rooms.splice(0, 0, user.rooms[indexOfGeneral]);
+        user.rooms.splice(indexOfGeneral+1, 1);
     }
     
     let updateResult = await database.collection("rooms").updateOne({name: "general"}, {$set: {users: general_users}});
